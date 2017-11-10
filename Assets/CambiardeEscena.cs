44592db8bdb3143403809070ab3e2b7161694622
Valeench10 <32395262+Valeench10@users.noreply.github.com>
2017-11-10 09:24:02 -0500

    Cautivo_V21

---
+++ Assets/CambiardeEscena.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiardeEscena : MonoBehaviour {
+
+    public int keyCount;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
